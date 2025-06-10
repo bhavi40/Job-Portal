46c957d9bbec082c012c30563dbf965ba682deed
bhavi40 <vudathabhavishya@gmail.com>
2025-06-09 21:22:14 -0600

    Day 1: Installed packeges and created backend Server

---
+++ Backend/index.js
@@ -0,0 +1,31 @@
+//creating server - 'mean that this file sets up and starts the backend server that will listen for incoming requests (like from your frontend).'
+// Add type:module in package.json To use the import in index.js like react style instead of writing an old way const express=require('express') 
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app=express();
+
+//creating api
+/*app.get("/home",(req,res)=>{
+    return res.status(200).json({
+        meessage:"I am coming from the backend",
+        success:true
+    })
+});*/
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+const corsOptions={
+    origin:'http//localhost:5173', //Frontend localhost
+    credentials:true
+}
+app.use(cors(corsOptions));
+
+const PORT=3000;
+app.listen(PORT, ()=>{
+    console.log(`Server Running at Port ${PORT}`)
+}
+)
