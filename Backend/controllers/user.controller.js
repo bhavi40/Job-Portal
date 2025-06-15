6722aee7afa116d23f98743745eac6b867c03f83
bhavi40 <vudathabhavishya@gmail.com>
2025-06-14 19:30:43 -0600

    Added login,logout,updateuser functions in UserController

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,5 +1,7 @@
+import { use } from "react";
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register=async(req,res)=>{
     try{
@@ -28,9 +30,120 @@ export const register=async(req,res)=>{
         phonenumber,
         password:hashedPassword,
         role
-       })
+       });
+       return res.status(200).json({
+        message:'Account Created Successfully.',
+        success:true
+       });
 
-    }catch{
+    }catch(error){
+        console.log(error);
 
+    }
+}
+
+export const login= async(req,res)=>{
+    try{
+        const {email,Password,role}=req.body;
+        if (!email || !Password ||!role){
+            return res.status(400).json({
+                message:'Something is missing.',
+                success:false
+            });
+        };
+        //check user there or not
+        let user= await User.findOne({email})
+        if(!user){
+            return res.status(400).json({
+                message:'Incorrect Email or Password',
+                success:false
+            })
+        }
+        //check password matches or not
+        const isPasswordMatch= await bcrypt.compare(password,user.password)
+        if(!isPasswordMatch){
+            return res.status(400).json({
+                message:'Incorrect Email or Password',
+                success:false
+            })
+        }
+        //check role correct or not
+        if (role != user.role){
+            return res.status(400).json({
+                message:"Account doesn't exist with Current role",
+                success:false
+            });
+        }
+        //Generating Token- u can decide what info to keep
+        const tokenData={
+            UserID:user._id //_id is automatically generated mongodb, it acts as primary key
+        }
+        const token=await jwt.sign(tokenData,process.env.SECRET_KEY,{expiresIn:'1d'})//expires in 1day
+
+        user={
+            _id:user._id,
+            fullname:user.fullname,
+            email:user.email,
+            phoneNumber:user.phonenumber,
+            role:user.role,
+            profile:user.role
+        }
+        //token is stored in cookies
+        return res.status(200).cookie("token",{maxage:1*24*60*60*1000, httpsOnly:true, sameSite:'strict'}).json({
+            message:`Welcome back ${user.fullname}`,
+            user,
+            success:true
+        })
+
+    }catch(error){
+        console.log(error);
+
+    }
+}
+
+export const logout=async(req,res)=>{
+    try{
+        //emptying the token
+        return res.status(200).cookie("token","",{maxage:0}).json({
+            message:"Logged Out Successfully",
+            success:true
+        })
+    }catch(error){
+      console.log(error);
+    }
+}
+
+export const updateProfile=async (req,res)=>{
+    try{
+        const {fullname,email,phonenumber,bio,skills}=req.body;
+        if (!fullname || !email || !phonenumber || !bio || !skills)
+        {
+            return res.status(400).json({
+                message:"Something is missing",
+                success:false
+            });
+       };
+
+       //skills will be in string format, we have to convert it into array
+       const skillsArray=skills.split(",");
+       //for updating as well we need to authenticate
+       const userId=req.id; //middleware authentication
+       let user=await user.findByID(UserID);
+       if(!user){
+        return res.status(400).json({
+            message:'User not found.',
+            success:false
+        })
+       }
+
+        user.fullname=fullname,
+        user.email=email,
+        user.phoneNumber=phonenumber,
+        user.profile.bio=bio,
+        user.profile.skills=skillsArray
+
+
+    }catch(error){
+        console.log(error);
     }
 }
